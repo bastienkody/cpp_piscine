34c55132fad1ad66250ca1a2916339fd429eab0a
Bastien Guillaume <bguillau@student.42.fr>
2023-12-11 21:01:23 +0100

    protected zombiehorde cpp01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -2,21 +2,25 @@
 
 void	announceHorde(int size, Zombie *horde)
 {
+	if (size <= 0)
+		return ;
 	for (int i = 0; i < size; i++)
 		horde[i].announce();
 }
 
 int	main(void)
 {
+	Zombie	*zombies_minus_1 = zombieHorde(-1, "Arghhhhhhhh");
 	Zombie	*zombies_0 = zombieHorde(0, "Joe");
 	Zombie	*zombies_1 = zombieHorde(1, "OzdenAkca");
 	Zombie	*zombies_2 = zombieHorde(2, "MariaTambien");
-	Zombie	*zombies_10 = zombieHorde(10, "LeGouvernementFrancaisMeFaitHonte");
+	Zombie	*zombies_10 = zombieHorde(10, "Meursault");
 
 	announceHorde(1, zombies_1);
 	announceHorde(2, zombies_2);
 	announceHorde(10, zombies_10);
 
+	delete [] zombies_minus_1;
 	delete [] zombies_0;
 	delete [] zombies_1;
 	delete [] zombies_2;
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -2,9 +2,20 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
-	Zombie	*zombies = new Zombie [N];
-
-	for (int i = 0; i < N; i++)
-		zombies[i].setName(name);
-	return (zombies);
+	try
+	{
+			Zombie	*zombies = new Zombie [N];
+			for (int i = 0; i < N; i++)
+				zombies[i].setName(name);
+			return (zombies);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what();
+		if (N < 0)
+			std::cerr << ": N can't be negative here, stupid user" << '\n';
+		else
+			std::cerr << ": feels like no more memory on the heap??" << '\n';
+		return (NULL);
+	}
 }

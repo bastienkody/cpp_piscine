fe1e5506f1752f03eee27e60363b7c3e4b88f978
Bastien Guillaume <bguillau@student.42.fr>
2024-05-20 15:58:10 +0200

    jacob pb corrected + offest possibiltiy"

--- _jacobPrint/main.cpp
+++ _jacobPrint/main.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 
+#define OFFSET 0 // can be used to get real nb used in mij algo (jacob +1)
+
 typedef unsigned int uint;
 
 void	jacobPrint(uint anteprev=0, uint prev=1, uint index=1)
@@ -8,19 +10,19 @@ void	jacobPrint(uint anteprev=0, uint prev=1, uint index=1)
 	uint	nb = prev + anteprev * 2;
 
 	if (index == 1)
-		std::cout << 'i' << index << " - " << 0 << std::endl;
+		std::cout << 'i' << index << " - " << 0 + OFFSET << std::endl;
 	else if (index == 2)
-		std::cout << 'i' << index << " - " << 1 << std::endl;
+		std::cout << 'i' << index << " - " << 1 + OFFSET << std::endl;
 	else if (index == 3)
-		std::cout << 'i' << index << " - " << 1 << std::endl;
+		std::cout << 'i' << index << " - " << 1 + OFFSET << std::endl;
 	else if (prev > nb)
 	{
 		std::cout << "\033[5mOverflow baby use more bits please\033[0m" << std::endl; 
 		return ;
 	}
 	else
-		std::cout << 'i' << index << " - " << nb << std::endl;
-	jacobPrint(prev, nb, ++index);
+		std::cout << 'i' << index << " - " << nb + OFFSET << std::endl;
+	index == 3 ? jacobPrint(anteprev, prev, ++index) : jacobPrint(prev, nb, ++index);
 }
 
 int	main(void)
